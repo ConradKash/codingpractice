8c871e5ca01c1e4321ca6d5e8fc5802cb19e8637
ConradKash <kakurucon1234@gmail.com>
2023-12-22 02:15:40 +0300

    Added tested for candies diistribution

--- leetcode/blind75/arrays/candiesDistribution.py
+++ leetcode/blind75/arrays/candiesDistribution.py
@@ -1,28 +1,39 @@
-def candiesDistribution(array,  candies):
+def candiesDistribution(array, candies):
+    """
+    Distributes candies to each element in the array and determines if the resulting value is greater than or equal to the maximum value in the array.
+
+    Args:
+        array (list): The input array of integers.
+        candies (int): The number of candies to distribute.
+
+    Returns:
+        tuple: A tuple containing three lists:
+            - results (list): A list of boolean values indicating if the resulting value is greater than or equal to the maximum value.
+            - resultsnum (list): A list of the resulting values after distributing candies.
+            - resultsint (list): A list of the original values from the input array.
+
+    """
+
     results = []
     maxvalue = 0
-    resultsnum = []
-    resultsint = []
+
+
+    # Find the maximum value in the array
     for candy in array:
         if maxvalue < candy:
             maxvalue = candy
-        
 
+    # Distribute candies and determine if the resulting value is greater than or equal to the maximum value
     for i in range(len(array)):
         x = array[i] + candies
-        resultsint.append(array[i])
         if x >= maxvalue:
-            resultsnum.append(x)
             results.append(True)
         else:
-            resultsnum.append(x)
             results.append(False)
-        
-    
-    return results, resultsnum, resultsint
+
+    return results
 
 if __name__ == "__main__":
     print(candiesDistribution([2, 3, 5, 1, 3], 3))
     print(candiesDistribution([4, 2, 1, 1, 2], 1))
     print(candiesDistribution([12, 1, 2], 10))
-    
--- leetcode/blind75/test/test_array.py
+++ leetcode/blind75/test/test_array.py
@@ -1,5 +1,6 @@
 from leetcode.blind75.arrays.mergearray import mergeAlternatively
 from leetcode.blind75.arrays.gcd_of_arrays import gcdOfStrings
+from leetcode.blind75.arrays.candiesDistribution import candiesDistribution
 
 def test_mergeAlternatively():
     assert mergeAlternatively(['a','f','g'], ['i','j','k']) == "aifjgk"
@@ -9,3 +10,8 @@ def test_gcdOfArrays():
     assert gcdOfStrings("ABABAB", "ABAB") == "AB"
     assert gcdOfStrings("LEET", "CODE") == ""
     assert gcdOfStrings("txxtxxtxx", "txx") == "txx"
+    
+def test_candiesDistribution():
+    assert candiesDistribution([2, 3, 5, 1, 3], 3) == [True, True, True, False, True]
+    assert candiesDistribution([4, 2, 1, 1, 2], 1) == [True, False, False, False, False]
+    assert candiesDistribution([12, 1, 3], 10) == [True, False, True]
