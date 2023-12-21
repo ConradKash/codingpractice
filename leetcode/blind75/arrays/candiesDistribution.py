ebf06ea7efd1022bdf590d03ef5d58bb23fb2709
ConradKash <kakurucon1234@gmail.com>
2023-12-22 01:59:07 +0300

    Error was in transversing the array to get the maximum number

--- leetcode/blind75/arrays/candiesDistribution.py
+++ leetcode/blind75/arrays/candiesDistribution.py
@@ -1,19 +1,17 @@
 def candiesDistribution(array,  candies):
     results = []
+    maxvalue = 0
     resultsnum = []
     resultsint = []
-    for i in range(1, len(array)):
-        key = array[i]
-        j = i - 1
-        while j >= 0 and array[j] > key:
-            array[j + 1] = array[j]
-            j -= 1
-        array[j + 1] = key
+    for candy in array:
+        if maxvalue < candy:
+            maxvalue = candy
+        
 
     for i in range(len(array)):
         x = array[i] + candies
         resultsint.append(array[i])
-        if x > array[-1]:
+        if x >= maxvalue:
             resultsnum.append(x)
             results.append(True)
         else:
@@ -26,5 +24,5 @@ def candiesDistribution(array,  candies):
 if __name__ == "__main__":
     print(candiesDistribution([2, 3, 5, 1, 3], 3))
     print(candiesDistribution([4, 2, 1, 1, 2], 1))
-    print(candiesDistribution([12, 1, 5], 10))
+    print(candiesDistribution([12, 1, 2], 10))
     
