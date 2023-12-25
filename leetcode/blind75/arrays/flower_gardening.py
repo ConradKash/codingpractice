3cdd42c7c656c25efc82fbbcde4848fe4a4fac6e
ConradKash <kakurucon1234@gmail.com>
2023-12-25 21:28:46 +0300

    Cracked the flowerpot blind75 challenge

--- leetcode/blind75/arrays/flower_gardening.py
+++ leetcode/blind75/arrays/flower_gardening.py
@@ -5,22 +5,20 @@ def canPlaceFlower(flowerbed, n):
     if len(flowerbed) == 1 and flowerbed[0] == 1:
         return False
     new_flower = 0
-    for i in range(len(flowerbed)):        
+    x = 0
+    for i in range(len(flowerbed)):
+        # Edge cases
+        if i == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
+            n -= 1
+        if i == len(flowerbed)-1 and flowerbed[i] == 0 and flowerbed[i-1] == 0:
+            n -= 1
+        # Middle cases
         if flowerbed[i] == 0:
             new_flower += 1
-        print( flowerbed[i], n)
-        print(new_flower)  
-                  
-        if new_flower/2 == 0:
-            n -= 1
-            new_flower = 0  
-             
-    if n == 0:
-        return True
-    else:
-        return False
-    
-if __name__ == "__main__":
-    flowerbed = [1,0,0,0,1,0]
-    n = 2
-    print(canPlaceFlower(flowerbed, n))
+        if flowerbed[i] == 1:
+            new_flower = 0
+        if new_flower >= 3:
+            if new_flower == 3 or new_flower == (x+2):
+                x = new_flower
+                n -= 1                         
+    return n <= 0
--- leetcode/blind75/test/test_array.py
+++ leetcode/blind75/test/test_array.py
@@ -1,6 +1,7 @@
 from leetcode.blind75.arrays.mergearray import mergeAlternatively
 from leetcode.blind75.arrays.gcd_of_arrays import gcdOfStrings
 from leetcode.blind75.arrays.candiesDistribution import candiesDistribution
+from leetcode.blind75.arrays.flower_gardening import canPlaceFlower
 
 def test_mergeAlternatively():
     assert mergeAlternatively(['a','f','g'], ['i','j','k']) == "aifjgk"
@@ -15,3 +16,7 @@ def test_candiesDistribution():
     assert candiesDistribution([2, 3, 5, 1, 3], 3) == [True, True, True, False, True]
     assert candiesDistribution([4, 2, 1, 1, 2], 1) == [True, False, False, False, False]
     assert candiesDistribution([12, 1, 3], 10) == [True, False, True]
+    
+def test_flowerbed():
+    assert canPlaceFlower([1,0,0,0,1,0,0,0,0,0,1,0,0], 4) == True
+    assert canPlaceFlower([0,0,0,0], 2) == True
