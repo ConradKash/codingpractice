bc56c2dbfe25c7de74f4dac0a0de993ae0137b03
ConradKash <kakurucon1234@gmail.com>
2024-01-03 12:06:41 +0300

    Used multiplication and corrected the string.

--- leetcode/daily/day3.py
+++ leetcode/daily/day3.py
@@ -1,14 +1,17 @@
-def laser_bank(floor):
+def laser_bank(bank):
     # Your code here
-    laser = 0
-    for  m in range(len(floor)):
-        for n in range(len(floor[m])):
-            if floor[m][n] == 1:
-                for p in range(len(floor[m+1])):
-                    floor[p][n] = 1
-                    laser += 1
-                
-        
-    return laser
+    #temp stores the number of 1s in the previous string
+    #ans stores the total number of beams
+    ans, temp = 0, 0
+    for s in bank:
+        #n stores the number of 1s in the current string
+        n = s.count('1')
+        #if the string has only zeros we skip it
+        if n == 0:
+            continue
+        #we add the number of beams that can be formed with the current string
+        ans += temp * n
+        temp = n
+    return ans    
 
-print(laser_bank(["011001","001000"])) # 2
+print(laser_bank(["011001","001010"])) # 2
