506498a9869fefeeb3fe92353b86f6e0f57e6f73
ConradKash <kakurucon1234@gmail.com>
2024-03-14 03:02:45 +0300

    Update function to handle edge case scenarios

--- go_Programming/variables/variables.go
+++ go_Programming/variables/variables.go
@@ -3,20 +3,28 @@ package main
 import "fmt"
 
 func main() {
-
 	var x = 5
 	x += 1
 	fmt.Println(x)
-	temperature()
-	
-	lengthconversion()
+
+	fmt.Print("What do you want to covert lenth/temperature: ")
+	var y string
+	fmt.Scanf("%s", &y)
+	fmt.Println(y)
+	if y == "T" {
+		temperature()
+	} else if y == "L" {
+		lengthconversion()
+	} else {
+		fmt.Println("Repeat")
+	}
 }
 
 func temperature() {
 	fmt.Print("Enter the length in feet: ")
 	var input float64
 	fmt.Scanf("%f", &input)
-	output := ((input-32)*5/9)
+	output := ((input - 32) * 5 / 9)
 	fmt.Println(output)
 }
 
@@ -24,7 +32,7 @@ func lengthconversion() {
 	const ft = 0.3048
 	var c float64
 	fmt.Print("Enter the length in feet: ")
-	fmt.Scanln("%f", &c)
+	fmt.Scanf("%f", &c)
 	output := c * ft
 	fmt.Println(output)
 }
