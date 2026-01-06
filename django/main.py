ef73dc54ce9a132f8861c9900c169c42d525a563
ConradKash <kakurucon1234@gmail.com>
2026-01-06 23:34:35 +0300

    Add main.py with multiply and add_numbers functions; update .gitignore to exclude .idea directory

---
+++ django/main.py
@@ -0,0 +1,17 @@
+# x = 1
+# y = 2
+# c = x + y
+# print(c)
+d = {1,2,3,4}
+e = 2
+
+def multiply(m, n):
+    f = []
+    for i in m:
+        k =i*n
+        f.append(k)
+    return f
+
+def add_numbers(a, b):
+    c = a + b
+    return c
